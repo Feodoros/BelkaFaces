e0e9d319352c8e2bad7af393c10974b5378e83de
Fedor Zhilkin <fzhilkin@mail.ru>
2020-05-23 22:32:04 +0300

    Add EmguCV

--- WiderFacesNET/WiderFacesNET/Program.cs
+++ WiderFacesNET/WiderFacesNET/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
 
 namespace WiderFacesNET
 {
@@ -10,6 +11,12 @@ namespace WiderFacesNET
     {
         static void Main(string[] args)
         {
+            string modelFile = "models/opencv_face_detector_uint8.pb";
+            string configFile = "models/opencv_face_detector.pbtxt";
+
+            var net = Emgu.CV.Dnn.DnnInvoke.ReadNetFromTensorflow(modelFile, configFile);
+
+            Console.WriteLine();
         }
     }
 }
